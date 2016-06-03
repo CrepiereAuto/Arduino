3af2d6ba2e51ded5d00e2f212f433d5c2a2782b3
Hougo13 <hougo13@gmail.com>
2016-06-03 08:09:57 +0200

    Stepper

--- Arduino.ino
+++ Arduino.ino
@@ -1,13 +1,30 @@
+#include <Stepper.h>
+
 int rsbReturn = 2;
 int rsb = 3;
 int rsbState = 0;
 int state = 0;
+int in1Pin = 12;
+int in2Pin = 11;
+int in3Pin = 10;
+int in4Pin = 9;
+int vsteps = 8000;
+
+#define STEPS 512
+Stepper motor(STEPS, in1Pin, in2Pin, in3Pin, in4Pin);
 
 void setup() {
   Serial.begin(9600);
+  
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
   pinMode(rsbReturn, OUTPUT);
   pinMode(rsb, INPUT);
+  
   attachInterrupt(digitalPinToInterrupt(rsb), cmd, RISING);
+  motor.setSpeed(60);
 }
 
 void loop() {
@@ -26,4 +43,12 @@ void cmd() {
 
 void sequence() {
   delay(5000);
+}
+
+void outVacuum() {
+    motor.step(vsteps);
+}
+
+void inVacuum() {
+    motor.step(-vsteps);
 }
